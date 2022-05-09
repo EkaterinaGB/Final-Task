be2a8548ac96be01f2703afd03f31641df9e6881
Ekaterina <ivanova.osipenko@mail.ru>
2022-05-09 15:36:55 +0300

    Добавлено решение задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array1 = new string[5] { "Russia", "050", "2022", "hello", ":-)" };
+string[] array2 = new string[array1.Length];
+Console.Write($"Новый сформированный массив: [");
+void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+       
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+FillArray(array1, array2);
+PrintArray(array2);
